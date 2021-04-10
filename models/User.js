4c019269f8f69f780579f0f2466e620ecfac0a7b
zozi <wjdwhwl1@gmail.com>
2021-04-10 20:18:35 +0900

    bcrpyt를 이용한 비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt'); // 비밀번호 암호화하는데 필요한 패키지
+const saltRounds = 10; // 10자리 salt
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -29,6 +32,24 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: { // 토큰 유효기간
         type: Number
+    }
+})
+
+// 유저 모델을 저장하기 전에, 암호화
+userSchema.pre('save', function (next) {
+    var user = this;
+    if (user.isModified('password')) { // 유저 비밀번호가 바뀔때만.
+        //비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else { // 비밀번호를 바꾸는게 아니라 다른 정보 수정일때.
+        next()
     }
 })
 
