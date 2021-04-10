df59b33ec4382b3b103e7992fb695c8885cff78a
zozi <wjdwhwl1@gmail.com>
2021-04-10 18:02:44 +0900

    비밀 정보 보호

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI
+} // heroku 서비스로 배포할 때
--- index.js
+++ index.js
@@ -4,13 +4,35 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const { User } = require("./models/User");
+const bodyParer = require('body-parser');
+
+const config = require('./config/key');
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://zozi:zozi1234@boilerplate.gu6gb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected..'))
     .catch(err => console.log(err))
 
+// application/x-www-form-urlencoded
+app.use(bodyParer.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParer.json());
 
-app.get('/', (req, res) => res.send('Hello World! ~하이~'))
+app.post('/register', (req, res) => {
+    // 회원가입 할때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터베이스에 넣어준다.
+    const user = new User(req.body)
+    user.save((err, userInfo) => { // mongoDB에서 오는 method : save
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({ // 200은 성공했다는 의미
+            success: true
+        })
+    })
+})
+
+app.get('/', (req, res) => res.send('Hello World! ~하이~~~'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
